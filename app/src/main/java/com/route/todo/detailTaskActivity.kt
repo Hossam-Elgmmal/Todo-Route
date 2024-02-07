83a528b9b27a4c89964fe1e5f7ba2b9b3eaf1411
Hossam-Elgmmal <109397447+Hossam-Elgmmal@users.noreply.github.com>
2024-02-07 10:30:05 +0200

    started task detail activity

---
+++ app/src/main/java/com/route/todo/detailTaskActivity.kt
@@ -0,0 +1,11 @@
+package com.route.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class detailTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_task)
+    }
+}
