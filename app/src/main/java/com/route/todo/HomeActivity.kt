8c95bd3fbb90f338285735c0e4c8756a22a0cefa
Hossam-Elgmmal <109397447+Hossam-Elgmmal@users.noreply.github.com>
2024-02-08 09:50:13 +0200

    added task fragment logic added settings fragment logic

--- app/src/main/java/com/route/todo/HomeActivity.kt
+++ app/src/main/java/com/route/todo/HomeActivity.kt
@@ -2,10 +2,43 @@ package com.route.todo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.route.todo.databinding.ActivityHomeBinding
+import com.route.todo.fragments.AddTaskFragment
+import com.route.todo.fragments.SettingsFragment
+import com.route.todo.fragments.TasksFragment
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addBtn.setOnClickListener {
+            val bottomSheet = AddTaskFragment()
+            bottomSheet.show(supportFragmentManager, null)
+        }
+        binding.navView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.tasks -> {
+                    pushFragment(TasksFragment())
+                }
+
+                R.id.settings -> {
+                    pushFragment(SettingsFragment())
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
+        binding.navView.selectedItemId = R.id.tasks
+    }
+
+    private fun pushFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(binding.content.fragmentContainer.id, fragment)
+            .commit()
+
     }
 }
